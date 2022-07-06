dc8e4091e6fb388093f81c0bc2b971d75fdabf9e
Haany Ali <asterp04@gmail.com>
2022-07-06 23:06:02 +0530

    redux init; login and register pages

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,83 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) =>
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log('Success');
+    // const newUser = {
+    //   name,
+    //   email,
+    //   password,
+    // };
+
+    // try {
+    //   const config = {
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //   };
+    //   const body = JSON.stringify(newUser);
+    //   const res = await axios.post('/api/users', body, config);
+    //   console.log(res.data);
+    // } catch (err) {
+    //   console.error(err.response.data);
+    // }
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Sign into your account
+      </p>
+      <form
+        action='dashboard.html'
+        className='form'
+        onSubmit={(e) => onSubmit(e)}
+      >
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            required
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Enter password'
+            minLength='6'
+            name='password'
+            value={password}
+            required
+            onChange={(e) => onChange(e)}
+          />
+        </div>
+
+        <input type='submit' value='Login' className='btn btn-primary' />
+      </form>
+      <p className='my-1'>
+        Don't have an account?
+        <Link to='/register'> Register</Link>
+      </p>
+    </Fragment>
+  );
 };
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
   const [formData, setFormData] = useState({
@@ -16,12 +17,30 @@ export const Register = () => {
       [e.target.name]: e.target.value,
     });
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
       console.log('Password do not match');
     } else {
-      console.log(formData);
+      console.log('Success');
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password,
+      // };
+
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //     },
+      //   };
+      //   const body = JSON.stringify(newUser);
+      //   const res = await axios.post('/api/users', body, config);
+      //   console.log(res.data);
+      // } catch (err) {
+      //   console.error(err.response.data);
+      // }
     }
   };
 
@@ -90,7 +109,7 @@ export const Register = () => {
       </form>
       <p className='my-1'>
         Already have an account?
-        <a href='login.html'>Sign in</a>
+        <Link to='/login'>Sign in</Link>
       </p>
     </Fragment>
   );
